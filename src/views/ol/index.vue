9a24aa3ade1059087348690e0fb61fe7dd40f1a2
thinkplayer <thinkplayer@126.com>
2021-03-02 09:12:29 +0800

    feat(ol): 简单实现ol初始化和feature的上图

--- src/views/ol/index.vue
+++ src/views/ol/index.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="container">
-    <div id="map"></div>
+    <div id="map">
+      <div class="fixed-wrap">
+        <el-button type="primary" @click="featureDrawer = true"
+        >feature
+        </el-button
+        >
+      </div>
+    </div>
+
+    <el-drawer :visible.sync="featureDrawer" :with-header="false">
+      <OLFeature @chooseFeatureType="chooseFeatureType"></OLFeature>
+    </el-drawer>
   </div>
 </template>
 
 <script>
 import 'ol/ol.css'
 import Map from 'ol/Map'
-import OSM from 'ol/source/OSM'
-import TileLayer from 'ol/layer/Tile'
+import Draw from 'ol/interaction/Draw'
+import { OSM, Vector as VectorSource } from 'ol/source'
 import View from 'ol/View'
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
 import { fromLonLat } from 'ol/proj'
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
+import { Modify, Snap } from 'ol/interaction'
+
+import OLFeature from './components/feature'
 
 export default {
   name: 'OL',
+  components: { OLFeature },
   data() {
     return {
-      map: null
+      featureDrawer: false,
+      map: null,
+      mapOpt: {
+        draw: null
+      }
     }
   },
   computed: {},
@@ -27,19 +48,63 @@ export default {
   },
   methods: {
     initMap() {
+      const raster = new TileLayer({
+        source: new OSM(),
+        zIndex: 0,
+        className: 'ol-baseMap'
+      })
       this.map = new Map({
         target: 'map',
-        layers: [
-          new TileLayer({
-            source: new OSM()
-          })
-        ],
+        layers: [raster],
         view: new View({
           // fromLonLat 将4326转成3857
           center: fromLonLat([113.658097, 34.745795]),
           zoom: 12
         })
       })
+      this.mapEvents()
+    },
+    mapEvents() {
+      this.map.on('click', ({ pixel }) => {
+        const feature = this.map.forEachFeatureAtPixel(pixel, v => v)
+        const layers = this.map.getLayers()
+        const featureLlayer = layers.array_.find(layer => layer.className_ === 'ol-feature')
+        const featureSource = featureLlayer.getSource()
+        const modify = new Modify({source: featureSource})
+        const snanp = new Snap({source: featureSource})
+        this.map.addInteraction(modify)
+        this.map.addInteraction(snanp)
+      })
+      this.map.on('pointermove', (evt) => {
+        if (evt.dragging) return
+      })
+    },
+
+    chooseFeatureType(type) {
+      this.drawFeature(type)
+    },
+
+    drawFeature(type) {
+      console.log('%c🌼type:', 'font-size:20px;border:3px inset #42b983', type)
+      const source = new VectorSource({ wrapX: false })
+      const featureLayer = new VectorLayer({
+        className: 'ol-feature',
+        source,
+        zIndex: 1
+      })
+      const layers = this.map.getLayers()
+      let { draw } = this.mapOpt
+      this.map.removeInteraction(draw)
+      if (type) {
+        draw = new Draw({
+          source,
+          type
+        })
+        this.map.addInteraction(draw)
+      }
+      if (!(layers.array_.some(layer => layer.className_ === 'ol-feature'))) {
+        this.map.addLayer(featureLayer)
+      }
     }
   }
 }
@@ -54,9 +119,18 @@ export default {
 #map {
   height: 100%;
   width: 100%;
+  position: relative;
 
   &:focus {
-    outline: #4A74A8 solid 0.15em;
+    outline: #4a74a8 solid 0.15em;
+  }
+
+  .fixed-wrap {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    padding: 10px;
+    z-index: 2;
   }
 }
 </style>
