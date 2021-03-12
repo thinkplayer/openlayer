c585b9141d464a8825e6bd706318ba764b7add18
thinkplayer <thinkplayer@126.com>
2021-03-12 13:49:15 +0800

    feature上图

--- src/views/ol/index.vue
+++ src/views/ol/index.vue
@@ -2,10 +2,9 @@
   <div class="container">
     <div id="map">
       <div class="fixed-wrap">
-        <el-button type="primary" @click="featureDrawer = true"
-        >feature
-        </el-button
-        >
+        <el-button type="primary" @click="featureDrawer = true">
+          feature
+        </el-button>
       </div>
     </div>
 
@@ -16,20 +15,25 @@
 </template>
 
 <script>
-import 'ol/ol.css'
-import Map from 'ol/Map'
-import Draw from 'ol/interaction/Draw'
-import { OSM, Vector as VectorSource } from 'ol/source'
-import View from 'ol/View'
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
-import { fromLonLat } from 'ol/proj'
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
-import { Modify, Snap } from 'ol/interaction'
+import "ol/ol.css";
+import Map from "ol/Map";
+import Draw from "ol/interaction/Draw";
+import { OSM, Vector as VectorSource } from "ol/source";
+import View from "ol/View";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { fromLonLat } from "ol/proj";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
+import {
+  Snap,
+  Modify,
+  Select,
+  defaults as defaultInteractions
+} from "ol/interaction";
 
-import OLFeature from './components/feature'
+import OLFeature from "./components/feature";
 
 export default {
-  name: 'OL',
+  name: "OL",
   components: { OLFeature },
   data() {
     return {
@@ -38,76 +42,92 @@ export default {
       mapOpt: {
         draw: null
       }
-    }
+    };
   },
   computed: {},
-  created() {
-  },
+  created() {},
   mounted() {
-    this.initMap()
+    this.initMap();
   },
   methods: {
     initMap() {
       const raster = new TileLayer({
         source: new OSM(),
         zIndex: 0,
-        className: 'ol-baseMap'
-      })
+        className: "ol-baseMap"
+      });
       this.map = new Map({
-        target: 'map',
+        target: "map",
         layers: [raster],
         view: new View({
           // fromLonLat 将4326转成3857
           center: fromLonLat([113.658097, 34.745795]),
           zoom: 12
         })
-      })
-      this.mapEvents()
+      });
+      this.mapEvents();
     },
     mapEvents() {
-      this.map.on('click', ({ pixel }) => {
-        const feature = this.map.forEachFeatureAtPixel(pixel, v => v)
-        const layers = this.map.getLayers()
-        const featureLlayer = layers.array_.find(layer => layer.className_ === 'ol-feature')
-        const featureSource = featureLlayer.getSource()
-        const modify = new Modify({source: featureSource})
-        const snanp = new Snap({source: featureSource})
-        this.map.addInteraction(modify)
-        this.map.addInteraction(snanp)
-      })
-      this.map.on('pointermove', (evt) => {
-        if (evt.dragging) return
-      })
+      this.map.on("click", evt => {
+        console.log(
+          "%c 🍔 evt: ",
+          "font-size:20px;background-color: #E41A6A;color:#fff;",
+          evt
+        );
+        const { pixel } = evt;
+        const feature = this.map.forEachFeatureAtPixel(pixel, v => v);
+        const layers = this.map.getLayers();
+        const featureLlayer = layers.array_.find(
+          layer => layer.className_ === "ol-feature"
+        );
+        if (!featureLlayer) return;
+        const featureSource = featureLlayer.getSource();
+        const modify = new Modify({ source: featureSource });
+        const snanp = new Snap({ source: featureSource });
+        this.map.addInteraction(modify);
+        this.map.addInteraction(snanp);
+      });
+      this.map.on("pointermove", evt => {
+        if (evt.dragging) return;
+      });
+      this.map.on("dbclick", evt => {
+        console.log(
+          "%c 🍑 evt: ",
+          "font-size:20px;background-color: #6EC1C2;color:#fff;",
+          evt
+        );
+      });
     },
 
     chooseFeatureType(type) {
-      this.drawFeature(type)
+      this.drawFeature(type);
     },
 
     drawFeature(type) {
-      console.log('%c🌼type:', 'font-size:20px;border:3px inset #42b983', type)
-      const source = new VectorSource({ wrapX: false })
+      console.log("%c🌼type:", "font-size:20px;border:3px inset #42b983", type);
+      const source = new VectorSource({ wrapX: false });
       const featureLayer = new VectorLayer({
-        className: 'ol-feature',
+        className: "ol-feature",
         source,
         zIndex: 1
-      })
-      const layers = this.map.getLayers()
-      let { draw } = this.mapOpt
-      this.map.removeInteraction(draw)
+      });
+      const layers = this.map.getLayers();
+      let { draw } = this.mapOpt;
+      this.map.removeInteraction(draw);
       if (type) {
         draw = new Draw({
           source,
           type
-        })
-        this.map.addInteraction(draw)
+        });
+        this.map.addInteraction(draw);
       }
-      if (!(layers.array_.some(layer => layer.className_ === 'ol-feature'))) {
-        this.map.addLayer(featureLayer)
+      // 如果没有feature的图层,就添加
+      if (!layers.array_.some(layer => layer.className_ === "ol-feature")) {
+        this.map.addLayer(featureLayer);
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
